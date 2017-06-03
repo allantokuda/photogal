3ca4adec70506b22dfbca5ae09b8ad195cfdbbf4
Allan Tokuda <praetis@gmail.com>
2017-06-03 15:21:58 -0500

    Really simple gallery build

--- build_gallery.rb
+++ build_gallery.rb
@@ -1,3 +1,26 @@
 #!/usr/bin/env ruby
 
-puts 'yes'
+gallery_paths = Dir.glob 'galleries/*'
+
+CSS = <<-CSS
+<style>
+  body {
+    background: #333;
+  }
+</style>
+CSS
+
+gallery_paths.each do |gallery_path|
+  index_path = File.join gallery_path, 'index.html'
+  image_paths = Dir.glob(File.join(gallery_path, '**/*.jpg'))
+  tags = image_paths.map { |p| File.basename p }.map do |path|
+    <<-HTML
+      <a href=#{path}>
+        <img src="#{path}" height=200>
+      </a>
+    HTML
+  end
+
+  File.write(index_path, CSS)
+  File.write(index_path, tags.join("\n"), mode: 'a')
+end
